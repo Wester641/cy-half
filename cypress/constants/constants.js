141036b9854cc1f98513576418472f86bcac6fb7
zafarzhon <zafarzhon77@gmail.com>
2024-12-01 17:32:08 +0600

    Fixed create-unit function in units page

---
+++ cypress/constants/constants.js
@@ -0,0 +1,27 @@
+export const stateRegistration = [
+  "Illinois",
+  "Indiana",
+  "Wisconsin",
+  "Iowa",
+  "Missouri",
+  "Michigan",
+  "Ohio",
+  "Minnesota",
+  "Kentucky",
+  "Michigan",
+];
+
+export const truckColors = [
+  "White",
+  "Black",
+  "Silver",
+  "Red",
+  "Blue",
+  "Gray",
+  "Yellow",
+  "Green",
+  "Orange",
+  "Brown",
+];
+export const trimTrucks = ["SE", "LE", "XLE"];
+export const truckMsrpRanges = ["80000", "120000", "140000", "180000"];
